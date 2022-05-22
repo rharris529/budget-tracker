45f27b220497be0ef7335645dc6d51ccda3a8592
rharris529 <jordandine@gmail.com>
2022-05-22 13:38:55 -0500

    added service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,65 @@
-const APP_PREFRIX = 'Bug'
+const APP_PREFRIX = 'BudgetApp-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFRIX + VERSION;
+
+const FILES_TO_CACHE = [
+    './index.html',
+    './js/index.js',
+    './js/idb.js',
+    './css/styles.css',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png'
+];
+
+// Cache resources
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('install cache :' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+// Activate service-worker.js
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then( function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFRIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache :' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+});
+
+// Fetch Request
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request :' + e.request.url)
+    e.respondsWith(
+        caches.match(e.request).then(function(request) {
+            if(request) {
+                console.log('responding with cache :' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching :' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
